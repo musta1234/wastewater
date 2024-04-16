063727c3948418c0efd3a6499200ea4ce1e72f7a
musta1234 <musta1234@yahoo.com>
2024-04-16 18:52:55 -0400

    April 16, 2024

--- Code/next_Saturday_function.R
+++ Code/next_Saturday_function.R
@@ -1,18 +1,23 @@
 
 #function to create epiweek
-days <- 1:3650 #approx 10 years
-date <- as.Date("2018-01-01") + days
-epi_week <- epiweek(date)
+#days <- 1:3650 #approx 10 years
+#date <- as.Date("2018-01-01") + days
+#epi_week <- epiweek(date)
 
 #all_days <- 
 #  c("Sunday", "Monday","Tuesday","Wednesday", "Thursday","Friday", "Saturday")
 
-df <- 
-  data.frame(date, epi_week) %>%
-  mutate(next_saturday = date + 7 - match(weekdays(date), 
-                 c("Sunday", "Monday","Tuesday","Wednesday",
-                   "Thursday","Friday", "Saturday")))
+#df <- 
+#  data.frame(date, epi_week) %>%
+#  mutate(next_saturday = date + 7 - match(weekdays(date), 
+#                                          c("Sunday", "Monday","Tuesday","Wednesday",
+#                                            "Thursday","Friday", "Saturday")))
 
-#match statement is used to find the index of the day of the week 
-# Sunday = 1, Monday = 2, etc. 
+
+#this function returns the next Saturday from a given date
+next_saturday <- function(date){
+  date + 7 - match(weekdays(date), 
+                   c("Sunday", "Monday","Tuesday","Wednesday",
+                     "Thursday","Friday", "Saturday"))
+}
 
