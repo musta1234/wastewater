a719645d8c5cd4043ea4d4dd966841f8d7e38301
musta1234 <musta1234@yahoo.com>
2024-09-27 10:20:58 -0400

    Sept 27 2024

--- Code/playground.R
+++ Code/playground.R
@@ -1,24 +1,51 @@
 install.packages("seasonal")
 library(seasonal)
+library(dplyr)
+
+c <- c("forecast", "seasonal", "purrr", "fable", "fabletools", "feasts",
+       "tsibble", "tsibbledata")
+for (y in c) install.packages(y, dep = TRUE)
+sapply(c, library, character.only=TRUE)
+
 # select all numeric variables from dataframe usa_dataset
+
+usa_tsibble <- 
+  usa_dataset %>% 
+  select(date, subvariant, viral_load, hosp_current, hosp_new, deaths) %>%
+  #convert subvariant to factor
+  mutate(subvariant = factor(subvariant, 
+            levels = c("BA.1", "BA.2", "BA.4 BA.5", "XBB", "JN.1"))
+         ) %>% 
+  as_tsibble() #%>% View()
+
+View(usa_tsibble)
+
 usa_ts <- 
-  cbind(date = usa_dataset[,1],
-        usa_dataset[, sapply(usa_dataset, is.numeric)]
-        ) %>% ts(frequency = 52, start = c(2022, 1)) #%>% View()
+  usa_dataset[ , c("date", "subvariant", "viral_load", "hosp_current", 
+                  "hosp_new", "deaths") 
+              ] %>% 
+  ts(frequency = 52.18, start = c(2022, 1)) #%>% View()
+
+View(usa_ts)
 
 # run classical decomposition of time series data usa_ts[ , 'viral_load']
 usa_decomp_ww <- decompose(usa_ts[, c('viral_load')], type='additive')
 autoplot(usa_decomp_ww)
 
-usa_decomp_hosp <- seas(usa_ts[, c('hosp_new')], )
+usa_decomp_hosp <- decompose(usa_ts[, c('hosp_new')], )
 autoplot(usa_decomp_hosp)
 
 # run X11 decomposition of time series data usa_ts[ , 'viral_load']
 
-usa_decomp_x11 <- seas(usa_ts[, c('viral_load')], x11 = "", outlier = "auto")
+usa_decomp_x11 <- seas(usa_ts[, c('date', 'viral_load')], x11 = "", outlier = "auto")
 
 autoplot(usa_decomp_x11)
 
+library(fable)
+library(feasts)
+library(tsibble)
+usa_tsibble %>%
+  model(x11 = X_13ARIMA_SEATS(.data$viral_load ~ x11()))
 
 
 #Model 7: ARIMA(1,1,1) with drift
